198edfb1295bb70bc655dba989ab8d16e29dc3e0
Your Name <deep414@gmail.com>
2012-06-18 15:03:41 +0400

    Just test

--- testGit/src/TestGit.java
+++ testGit/src/TestGit.java
@@ -10,5 +10,6 @@ public class TestGit {
 		System.out.println("Hello, git!");
 		System.out.println("check gut hub functionallity");
 		System.out.println("Now check from another side");
+		// TODO just test
 	}
 }
